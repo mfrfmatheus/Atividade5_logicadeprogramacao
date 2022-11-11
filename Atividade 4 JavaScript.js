4cecf79bca93db809567194857c2542e598ca0ee
mfrfmatheus <115051947+mfrfmatheus@users.noreply.github.com>
2022-11-10 22:05:45 -0300

    Add files via upload

---
+++ Atividade 4 JavaScript.js
@@ -0,0 +1,24 @@
+let idade
+idade = 22
+if (idade>17){
+    console.log("idade validada com sucesso!")
+}else{
+    console.log("idade inválida para cadastro!")
+
+}
+var datadoevento
+datadoevento = 25
+if (datadoevento>10){
+    console.log("Data disponível para cadastro!")
+}else{
+    console.log("Cadastro invalido, informe uma data futura!")
+
+}
+var listadeparticipantes
+listadeparticipantes = ["Matheus","Thiago,Edgard","Elisagela","Morgana","Ragnar","Ariane"]
+if (listadeparticipantes.length<100){
+    console.log("cadastro efetuado com sucesso!")
+}else{
+    console.log("Cadastro não efetuado! excedido limite de partcipantes.")
+
+}
